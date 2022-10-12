86747594fa92f2991d1d2ca68d4130f2c68c6f28
DiptaPal <diptapal1998@gmail.com>
2022-10-12 17:47:56 +0600

    final update with option hight adjust

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -18,7 +18,7 @@ const Option = ({option, handleSelectBtn, correctAnswer, handleDisable, disable}
         <div>
             <div className='text-lg text-start'>
                 <button 
-                className= {`border border-indigo-500 py-2 px-3 rounded-md shadow-md mb-3 flex justify-center items-center cursor-pointer h-auto w-full ${isSelected ? `${isCorrect ? 'bg-green-500' : 'bg-red-600 text-white'} ` : 'bg-none hover:bg-indigo-200'}`}
+                className= {`border border-indigo-500 py-2 px-3 rounded-md shadow-md mb-3 flex justify-center items-center cursor-pointer lg:h-[100px] w-full ${isSelected ? `${isCorrect ? 'bg-green-500' : 'bg-red-600 text-white'} ` : 'bg-none hover:bg-indigo-200'}`}
                 
                 onClick={()=>{handleSelectBtn(option);handleCorrect(option); setSelected(true); handleDisable(true)}}
                 disabled={disable}
